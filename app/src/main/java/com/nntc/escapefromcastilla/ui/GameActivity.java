a88c8bfee6053791de884952082d7cb49fb870ed
Ilya Nikitin <f1uctus@mail.ru>
2020-09-25 00:32:30 +0300

    Bind `Actor` moving logic to arrow buttons

--- app/src/main/java/com/nntc/escapefromcastilla/ui/GameActivity.java
+++ app/src/main/java/com/nntc/escapefromcastilla/ui/GameActivity.java
@@ -1,15 +1,32 @@
 package com.nntc.escapefromcastilla.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.nntc.escapefromcastilla.Actor;
 import com.nntc.escapefromcastilla.R;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void onUpClick(View view) {
+        Actor.get().moveBy(0, -1);
+    }
+
+    public void onLeftClick(View view) {
+        Actor.get().moveBy(-1, 0);
+    }
+
+    public void onRightClick(View view) {
+        Actor.get().moveBy(1, 0);
+    }
+
+    public void onDownClick(View view) {
+        Actor.get().moveBy(0, 1);
     }
 }
