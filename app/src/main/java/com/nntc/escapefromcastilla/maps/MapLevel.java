6e8037fa3d7da124656863845017b01d553f6e2b
Ilya Nikitin <f1uctus@mail.ru>
2020-09-25 09:53:42 +0300

    Code cleanup

--- app/src/main/java/com/nntc/escapefromcastilla/maps/MapLevel.java
+++ app/src/main/java/com/nntc/escapefromcastilla/maps/MapLevel.java
@@ -2,7 +2,6 @@ package com.nntc.escapefromcastilla.maps;
 
 public class MapLevel {
     public static final int ground = 0;
-    public static final int mountain = 1;
 
     public static final int height = 10;
 }
--- app/src/main/java/com/nntc/escapefromcastilla/objects/Wall.java
+++ app/src/main/java/com/nntc/escapefromcastilla/objects/Wall.java
@@ -1,11 +1,9 @@
 package com.nntc.escapefromcastilla.objects;
 
-import com.crown.common.utils.Random;
 import com.crown.maps.Map;
 import com.crown.maps.MapIcon;
 import com.crown.maps.MapObject;
 import com.crown.maps.Point3D;
-import com.nntc.escapefromcastilla.maps.MapLevel;
 import com.nntc.escapefromcastilla.ui.MapIcons;
 
 public class Wall extends MapObject {
