747b070efeeed452a81dc67a3932354efbb41bdd
Ilya Nikitin <f1uctus@mail.ru>
2020-09-25 09:34:07 +0300

    Replace arrow buttons with joystick

--- app/src/main/java/com/nntc/escapefromcastilla/ui/GameActivity.java
+++ app/src/main/java/com/nntc/escapefromcastilla/ui/GameActivity.java
@@ -2,31 +2,35 @@ package com.nntc.escapefromcastilla.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.nntc.escapefromcastilla.Actor;
 import com.nntc.escapefromcastilla.R;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class GameActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-    }
 
-    public void onUpClick(View view) {
-        Actor.get().moveBy(0, -1);
-    }
-
-    public void onLeftClick(View view) {
-        Actor.get().moveBy(-1, 0);
-    }
-
-    public void onRightClick(View view) {
-        Actor.get().moveBy(1, 0);
-    }
-
-    public void onDownClick(View view) {
-        Actor.get().moveBy(0, 1);
+        ((JoystickView) findViewById(R.id.joystick)).setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                if (angle <= 225 && angle >= 135) {
+                    // left
+                    Actor.get().moveBy(-1, 0);
+                } else if (angle <= 315 && angle >= 225) {
+                    // down
+                    Actor.get().moveBy(0, 1);
+                } else if (angle <= 135 && angle >= 45) {
+                    // up
+                    Actor.get().moveBy(0, -1);
+                } else {
+                    // right
+                    Actor.get().moveBy(1, 0);
+                }
+            }
+        }, 200);
     }
 }
