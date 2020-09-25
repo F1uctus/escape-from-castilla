3978e2e06c1682be93ee62ea13a55172f0f7fbdf
Ilya Nikitin <f1uctus@mail.ru>
2020-09-26 00:44:09 +0300

    Improve joystick movement

--- app/src/main/java/com/nntc/escapefromcastilla/ui/GameActivity.java
+++ app/src/main/java/com/nntc/escapefromcastilla/ui/GameActivity.java
@@ -17,20 +17,22 @@ public class GameActivity extends Activity {
         ((JoystickView) findViewById(R.id.joystick)).setOnMoveListener(new JoystickView.OnMoveListener() {
             @Override
             public void onMove(int angle, int strength) {
-                if (angle <= 225 && angle >= 135) {
-                    // left
-                    Actor.get().moveBy(-1, 0);
-                } else if (angle < 135 && angle >= 45) {
-                    // up
-                    Actor.get().moveBy(0, -1);
-                } else if (angle <= 315 && angle > 225) {
-                    // down
-                    Actor.get().moveBy(0, 1);
-                } else {
-                    // right
-                    Actor.get().moveBy(1, 0);
+                if (strength > 40) {
+                    if (angle <= 225 && angle >= 135) {
+                        // left
+                        Actor.get().moveBy(-1, 0);
+                    } else if (angle < 135 && angle >= 45) {
+                        // up
+                        Actor.get().moveBy(0, -1);
+                    } else if (angle <= 315 && angle > 225) {
+                        // down
+                        Actor.get().moveBy(0, 1);
+                    } else {
+                        // right
+                        Actor.get().moveBy(1, 0);
+                    }
                 }
             }
-        }, 200);
+        }, 150);
     }
 }
