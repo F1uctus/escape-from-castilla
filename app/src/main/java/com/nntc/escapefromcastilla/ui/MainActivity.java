7307e1e0cb7f4c144826a765a99119c1e49f083e
Ilya Nikitin <f1uctus@mail.ru>
2020-09-28 21:35:26 +0300

    Interface improvements

--- app/src/main/java/com/nntc/escapefromcastilla/ui/MainActivity.java
+++ app/src/main/java/com/nntc/escapefromcastilla/ui/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,14 +12,21 @@ import com.crown.i18n.I18n;
 import com.nntc.escapefromcastilla.R;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class MainActivity extends AppCompatActivity {
     public static final HashMap<String, ResourceBundle> bundles = new HashMap<>();
+    public static final String locale = Locale.getDefault().getLanguage();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         setContentView(R.layout.activity_main);
 
         Log.d("Icons", "Initializing...");
@@ -41,4 +49,23 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
+
+//    /**
+//     * Using reflection to override default typeface
+//     * NOTICE: DO NOT FORGET TO SET TYPEFACE FOR APP THEME AS DEFAULT TYPEFACE WHICH WILL BE OVERRIDDEN
+//     * @param context to work with assets
+//     * @param defaultFontNameToOverride for example "monospace"
+//     * @param customFontFileNameInAssets file name of the font from assets
+//     */
+//    public static void overrideFont(Context context, String defaultFontNameToOverride, String customFontFileNameInAssets) {
+//        try {
+//            final Typeface customFontTypeface = Typeface.createFromAsset(context.getAssets(), customFontFileNameInAssets);
+//
+//            final Field defaultFontTypefaceField = Typeface.class.getDeclaredField(defaultFontNameToOverride);
+//            defaultFontTypefaceField.setAccessible(true);
+//            defaultFontTypefaceField.set(null, customFontTypeface);
+//        } catch (Exception e) {
+//            Log.e("Can not set custom font " + customFontFileNameInAssets + " instead of " + defaultFontNameToOverride);
+//        }
+//    }
 }
