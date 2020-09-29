d89380f75d831549f6f83669c3402054770d6b7d
Ilya Nikitin <f1uctus@mail.ru>
2020-09-29 08:42:11 +0300

    Icons system improvement

--- app/src/main/java/com/nntc/escapefromcastilla/creatures/Wanderer.java
+++ app/src/main/java/com/nntc/escapefromcastilla/creatures/Wanderer.java
@@ -1,12 +1,22 @@
 package com.nntc.escapefromcastilla.creatures;
 
-import com.crown.maps.*;
+import com.crown.maps.Point3D;
 import com.nntc.escapefromcastilla.GameState;
 import com.nntc.escapefromcastilla.R;
 import com.nntc.escapefromcastilla.ui.MapIcons;
 
 public class Wanderer extends Human {
-    public Wanderer(String name, GameState gs, Point3D pt) {
-        super(name, gs.getGlobalMap(), MapIcons.add(R.drawable.coin_gold, gs.context), pt);
+        public Wanderer(String name, GameState gs, Point3D pt) {
+        super(
+            name,
+            gs.getGlobalMap(),
+            MapIcons.add(
+                R.drawable.player_l,
+                R.drawable.player_u,
+                R.drawable.player_d,
+                4,
+                gs.context
+            ), pt
+        );
     }
 }
