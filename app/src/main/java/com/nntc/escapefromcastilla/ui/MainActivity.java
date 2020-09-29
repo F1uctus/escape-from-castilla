b80afe4c3fac503dbcf0ac3ada37d1aa92e7698a
Ilya Nikitin <f1uctus@mail.ru>
2020-09-29 08:46:27 +0300

    Remove dead code

--- app/src/main/java/com/nntc/escapefromcastilla/ui/MainActivity.java
+++ app/src/main/java/com/nntc/escapefromcastilla/ui/MainActivity.java
@@ -49,23 +49,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
-
-//    /**
-//     * Using reflection to override default typeface
-//     * NOTICE: DO NOT FORGET TO SET TYPEFACE FOR APP THEME AS DEFAULT TYPEFACE WHICH WILL BE OVERRIDDEN
-//     * @param context to work with assets
-//     * @param defaultFontNameToOverride for example "monospace"
-//     * @param customFontFileNameInAssets file name of the font from assets
-//     */
-//    public static void overrideFont(Context context, String defaultFontNameToOverride, String customFontFileNameInAssets) {
-//        try {
-//            final Typeface customFontTypeface = Typeface.createFromAsset(context.getAssets(), customFontFileNameInAssets);
-//
-//            final Field defaultFontTypefaceField = Typeface.class.getDeclaredField(defaultFontNameToOverride);
-//            defaultFontTypefaceField.setAccessible(true);
-//            defaultFontTypefaceField.set(null, customFontTypeface);
-//        } catch (Exception e) {
-//            Log.e("Can not set custom font " + customFontFileNameInAssets + " instead of " + defaultFontNameToOverride);
-//        }
-//    }
 }
